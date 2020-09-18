595f1905932baddb91c6a1b5dcdbe53b86989df3
CFNeko <55338055+CFNeko@users.noreply.github.com>
2020-09-17 21:38:42 -0400

    fsad

---
+++ history/cultures/Nyan.py
@@ -0,0 +1,34 @@
+from pathlib import Path
+path = Path.cwd()
+
+files = [e for e in path.iterdir() if e.is_file()]
+print(files)
+
+
+for file in files:
+	with open(file, 'a') as f:
+		f.write("""
+1444.11.10 = {
+	discover_innovation = innovation_currency_03
+	discover_innovation = innovation_divine_right
+	discover_innovation = innovation_guilds
+	discover_innovation = innovation_heraldry
+	discover_innovation = innovation_land_grants
+	discover_innovation = innovation_scutage
+	discover_innovation = innovation_development_03
+	discover_innovation = innovation_windmills
+
+	discover_innovation = innovation_advanced_bowmaking
+	discover_innovation = innovation_castle_baileys
+	discover_innovation = innovation_hoardings
+	discover_innovation = innovation_knighthood
+	discover_innovation = innovation_men-at-arms
+	discover_innovation = innovation_trebuchet
+
+	join_era = culture_era_high_medieval
+	join_era = culture_era_late_medieval
+}
+			""")
+
+
+			
